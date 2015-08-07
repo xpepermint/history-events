5a9151ab606c15ccdbb2899c9a7714d3c93fe061
xpeper <xpepermint@gmail.com>
2015-08-07 16:07:36 +0200

    adding module

---
+++ index.js
@@ -0,0 +1,80 @@
+(function() {
+
+  function triggerEvent(el, eventName, data) {
+    var event;
+    if (document.createEvent) {
+      event = document.createEvent('HTMLEvents');
+      event.initEvent(eventName, true, true);
+    } else if (document.createEventObject){// IE < 9
+      event = document.createEventObject();
+      event.eventType = eventName;
+    }
+    event.data = data;
+    event.eventName = eventName;
+    if (el.dispatchEvent) {
+      el.dispatchEvent(event);
+    } else if(el.fireEvent && htmlEvents['on'+eventName]) {// IE < 9
+      el.fireEvent('on'+event.eventType, event);// can trigger only real event (e.g. 'click')
+    } else if(el[eventName]) {
+      el[eventName]();
+    } else if(el['on'+eventName]) {
+      el['on'+eventName]();
+    }
+  }
+
+  function addEventListener(el, type, handler){
+    if (el.addEventListener) {
+      el.addEventListener(type, handler, false);
+    } else if (el.attachEvent && htmlEvents['on'+type]) {// IE < 9
+      el.attachEvent('on'+type, handler);
+    } else {
+      el['on'+type]=handler;
+    }
+  }
+
+  function removeEventListener(el, type, handler){
+    if (el.removeventListener) {
+      el.removeEventListener(type, handler, false);
+    } else if (el.detachEvent && htmlEvents['on'+type]) {// IE < 9
+      el.detachEvent('on'+type, handler);
+    } else {
+      el['on'+type]=null;
+    }
+  }
+
+  function isHistorySupported() {
+    return typeof window != 'undefined' && window.history && 'pushState' in window.history;
+  }
+
+  if (isHistorySupported()) {
+    var history = window.history;
+
+    var pushState = history.pushState;
+    history.pushState = function(state) {
+      triggerEvent(window, 'pushstate', {state: state});
+      triggerEvent(window, 'changestate', {state: state});
+      return pushState.apply(history, arguments);
+    }
+
+    var replaceState = history.replaceState;
+    history.replaceState = function(state) {
+      triggerEvent(window, 'replacestate', {state: state});
+      triggerEvent(window, 'changestate', {state: state});
+      return replaceState.apply(history, arguments);
+    }
+
+    addEventListener(window, 'popstate', function(state) {
+      triggerEvent(window, 'changestate', {state: state});
+    });
+  }
+
+  if (typeof module != 'undefined') {
+    module.exports = {
+      isHistorySupported: isHistorySupported,
+      addEventListener: addEventListener,
+      removeEventListener: removeEventListener,
+      triggerEvent: triggerEvent
+    };
+  }
+
+})();
